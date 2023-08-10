c4d702ed88c550313fe86996f1e85b29b70b8ce8
vlorian <vlorian.dervishi@gmail.com>
2023-08-10 13:26:43 -0400

    render book passed on to html

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   fetch("https://anapioficeandfire.com/api/books")
     .then((resp) => resp.json())
-    .then((json) => console.log(json));
+    .then((json) => renderBooks(json));
 }
 
 function renderBooks(books) {
